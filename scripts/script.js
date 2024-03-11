0ac5d0ea28852fbdca552df8c4305b610e0f2403
Leonardo Lauermann <leonardo.lauermann@safeweb.com.br>
2024-03-11 18:20:56 -0300

    .

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,11 @@ const modalCart = document.querySelector('#modal_cart');
 const closeModalCart = document.querySelector('#close-modal-btn');
 
 btnOpenCart.addEventListener('click', () => {
-  modalCart.style.display = 'flex';
+  modalCart.classList.add('flex');
+  modalCart.classList.remove('hidden');
 });
 
 closeModalCart.addEventListener('click', () => {
-  modalCart.style.display = 'none';
+  modalCart.classList.add('hidden');
+  modalCart.classList.remove('flex');
 });
