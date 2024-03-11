c575fa2001d86c56eedfdae595447c95dba71a2f
Leonardo Lauermann <leonardo.lauermann@safeweb.com.br>
2024-03-11 16:25:01 -0300

    fonte

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./**/*.{html,js}'],
   theme: {
+    fontFamily: {
+      'sans': ['Poppins'],
+    },
     extend: {
       backgroundImage: {
         home: 'url("/assets/bg.png")',
